d9e8329f1bd12185d58092851a63a2a53be9424a
HASH1da1 <d4y3re4k@gmail.com>
2019-06-06 16:02:35 +0900

    delete some typo

--- js/handler.js
+++ js/handler.js
@@ -1,14 +1,3 @@
 // This js is event handler
 
 //image changer
-//function getId(id_value){
-    //if(id_value = "blog-image"){
-        //id_value.src = "./images/w_hatena.png"
-    //}
-    //else if(id_value = "twitter-image"){
-        //id_value.src = "./images/w_twitter.png"
-    //}
-    //else if(id_valuelue = "mail-image"){
-        //id_value.src = "./images/w_mail.png"
-   // }
-    //};
